b89b6d944844ff5250e848321a06fb99fa435675
Sergey Grigorash <adviser@bk.ru>
2023-04-02 13:15:08 +0500

    refactor: отделил переменные для PopUpProfile

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,23 @@
-const popUp = document.querySelector('.popup');
-const crossToClose = popUp.querySelector('.popup__close-cross');
+// ПЕРЕМЕННЫЕ ДЛЯ POPUP PROFILE
+const popUpProfile = document.querySelector('.popup-profile');
+const crossToClosePopUpProfile = popUpProfile.querySelector('.popup__close-cross');
 const editButton = document.querySelector('.profile__edit-button');
 //уже имеющиеся в профиле имя и статус
 const currentName = document.querySelector('.profile__title');
 const currentStatus = document.querySelector('.profile__subtitle');
 //имя и статус в формах для заполнения попапа
-const nameInput = popUp.querySelector(".popup__input_type_name");
-const nameStatus = popUp.querySelector(".popup__input_type_status");
+const nameInput = popUpProfile.querySelector(".popup__input_type_name");
+const nameStatus = popUpProfile.querySelector(".popup__input_type_status");
 //ссылка на форму из попапа
-const formElement = popUp.querySelector('.popup__form');
+const formElement = popUpProfile.querySelector('.popup__form');
+
+// ПЕРЕМЕННЫЕ ДЛЯ POPUP NEW CARD
+
+
 
 
 /*************************************** */
-
+// ОТРИСОВКА КАРТОЧЕК
 
 //изначальные карточки
 const initialCards = [
@@ -62,14 +67,14 @@ initialCards.forEach((item) => {
 
 /******************** */
 
+//ФУНКЦИИ ДЛЯ ПОПАПА
 
-
-// функция открывает окно попап
+// общая функция открывает окно попап
 function popupToOpen(pop) {
   pop.classList.add('popup_opened');
 }
 
-// функция закрывает окно попап
+// общая функция закрывает окно попап
 function popupToClose(pop) {
   pop.classList.remove('popup_opened');
 }
@@ -79,19 +84,19 @@ function handleFormSubmit(evt) {
   evt.preventDefault();
   currentName.textContent = nameInput.value;
   currentStatus.textContent = nameStatus.value;
-  popupToClose(popUp);
+  popupToClose(popUpProfile);
 }
 
 //слушатель события клика на кнопку editButton
 editButton.addEventListener('click', () => {
-  popupToOpen(popUp);
+  popupToOpen(popUpProfile);
   nameInput.value = currentName.textContent;
   nameStatus.value = currentStatus.textContent;
 });
 
 //слушатель события клика на крестик закрывания попапа
-crossToClose.addEventListener('click', () => {
-  popupToClose(popUp);
+crossToClosePopUpProfile.addEventListener('click', () => {
+  popupToClose(popUpProfile);
 });
 
 //слушатель события нажатия на кнопку "Сохранить"
