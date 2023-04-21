5abf45df23896390fd32276f0cc1fa6341451b65
Sergey Grigorash <adviser@bk.ru>
2023-04-21 10:58:51 +0500

    feat: начал делать побуквенную валидацию

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,21 +1,45 @@
-//делаем массив formList на все формы в документе
-const formList = Array.from(document.querySelectorAll('.popup__form'));
+//функция проверки каждого вводимого в поле символа
+function checkInputValidity(input) {
+  //спорненько, что тут глобальный поиск, но пока лучше не придумал
+  const errorElement = document.querySelector(`#error-${input.id}`);
+
+  if (input.validity.valid) {
+    //если валидно
+    input.classList.remove('popup__input_invalid');
+    errorElement.textContent = '';
+
+  } else {
+    //если невалидно
+    input.classList.add('popup__input_invalid');
+    errorElement.textContent = input.validationMessage;
+  }
+
+};
 
 
-//делаем массив fieldsetList из всех input в массиве formList
-//сейчас в fieldsetList два массива - один из попапа Profile, второй - из попапа New Card
-formList.forEach((formElement) => {
-  const fieldsetList = Array.from(formElement.querySelectorAll('.popup__input'));
-  fieldsetList.forEach(function(input) {
-    input.addEventListener('input', function(e) {
+//общая функция проверки валидации
+function enableValidation() {
 
 
+  //делаем массив formList на все формы в документе
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
 
+  //делаем массив fieldsetList из всех input в массиве formList
+  //сейчас в fieldsetList два массива - один из попапа Profile, второй - из попапа New Card
+  formList.forEach((formElement) => {
+    const fieldsetList = Array.from(formElement.querySelectorAll('.popup__input'));
 
-    })
-  });
+    //проходимся по массиву fieldsetlist и на каждый ввод в input явно вызываем функцию checkInputValidity - валидация каждого вводимого символа
+    fieldsetList.forEach(function (input) {
+      //вешаем слушатель ввода на каждый ввод символа
+      input.addEventListener('input', () => {
 
-})
+        checkInputValidity(input);
 
+      })
+    });
+  })
 
+};
 
+enableValidation();
