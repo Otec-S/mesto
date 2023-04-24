c417f4524888ce65e1e49683372de3854b6f041e
Sergey Grigorash <adviser@bk.ru>
2023-04-24 10:14:22 +0500

    refact: почистил комменты

--- scripts/validate.js
+++ scripts/validate.js
@@ -3,28 +3,23 @@ function enableValidation() {
 
   //делаем массив formList на две формы в документе
   const formList = Array.from(document.querySelectorAll('.popup__form'));
-  console.log("formList это");
-  console.log(formList);
 
   //проходимся по массиву inputList и на каждый ввод в input явно вызываем функцию setEventListeners
   formList.forEach((formElement) => {
-
     setEventListeners(formElement);
   });
 };
 
 
-//ВТОРИЧНАЯ ФУНКЦИЯ ПРОВЕРКИ ВАЛИДНОСТИ (вероятно, для упрощения)
+//ВТОРИЧНАЯ ФУНКЦИЯ ПРОВЕРКИ ВАЛИДНОСТИ
 function setEventListeners(formElement) {
   //проходимся по формлисту и делаем массив из всех импутов
   const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
-  console.log("inputList это");
-  console.log(inputList);
-  //??? ссылка на кнопку текушего formElement
+
+  //ссылка на кнопку текушего formElement
   const buttonElement = formElement.querySelector('.popup__submit');
-  console.log("buttonElement это");
-  console.log(buttonElement);
-  // ???Вызовите функцию toggleButtonState после определения константы buttonElement. Это проверит состояние кнопки при первой загрузке страницы. Тогда кнопка перестанет быть активной до ввода данных в одно из полей.
+
+  //проверка состояния кнопки при первой загрузке страницы.
   toggleButtonState(inputList, buttonElement);
 
   //чистим форму после ее отправки и тут же проверяем кнопку на валидность, делая ее невалидной
@@ -39,35 +34,14 @@ function setEventListeners(formElement) {
     inputElement.addEventListener('input', function () {
       //вызываем функцию checkInputValidity
       checkInputValidity(formElement, inputElement);
-      // Вызовите функцию toggleButtonState в теле обработчика события input. Передайте ей массив полей формы и элемент кнопки. Такой вызов проверит состояние кнопки при каждом изменении символа в любом из полей.
+      //вызываем функцию toggleButtonState в теле обработчика события input. Передайкм ей массив полей формы и элемент кнопки. Такой вызов проверит состояние кнопки при каждом изменении символа в любом из полей.
       toggleButtonState(inputList, buttonElement);
     });
   });
 };
 
 
-
-
-
-
-/********************************** */
-
 //ВАЛИДАЦИЯ ПОЛЕЙ ВВОДА
-// //функция проверки каждого вводимого в поле символа
-// function checkInputValidity(input) {
-//   //спорненько, что тут глобальный поиск, но пока лучше не придумал
-//   const errorElement = document.querySelector(`#error-${input.id}`);
-
-//   if (input.validity.valid) {
-//     //если валидно
-//     setInputValidState(input, errorElement);
-
-//   } else {
-//     //если невалидно
-//     setInputInvalidState(input, errorElement);
-//   }
-// };
-
 //функция вызывает показ ошибок при невалидации
 function checkInputValidity(formElement, inputElement) {
   if (!inputElement.validity.valid) {
@@ -93,10 +67,6 @@ function hideInputError(formElement, inputElement) {
   errorElement.textContent = '';
 };
 
-
-
-/******************************************** */
-
 //ВАЛИДАЦИЯ КНОПОК ОТПРАВКИ
 //функция переключения состояния кнопки
 
@@ -110,7 +80,7 @@ function hasInvalidInput(inputList) {
 };
 
 //функция переключения состояния кнопок отправки
-// Первый параметр — массив полей, второй — кнопка «Далее».
+// Первый параметр — массив полей, второй — кнопка.
 function toggleButtonState(inputList, buttonElement) {
   if (hasInvalidInput(inputList)) {
     // сделай кнопку неактивной
@@ -123,33 +93,6 @@ function toggleButtonState(inputList, buttonElement) {
   }
 };
 
-// function toggleButtonValidity(input) {
-//   //получаем ссылку на массив из двух кнопок submit
-//   const submitButton = Array.from(document.querySelectorAll('.popup__submit'));
-//   //проверяем каждый элемент массива и меняем состяние кнопки от этого
-//   submitButton.forEach((buttonItem) => {
-//     if (input.checkValidity()) {
-//       enableButton(buttonItem);
-//     } else {
-//       disableButton(buttonItem);
-//     }
-//   })
-// };
-
-// //делаем кнопку отправки активной
-// function enableButton(button) {
-//   button.removeAttribute('disabled');
-//   button.classList.remove('popup__submit_disabled');
-// };
-
-// //делаем кнопку отправки неактивной
-// function disableButton(button) {
-//   button.setAttribute('disabled', '');
-//   button.classList.add('popup__submit_disabled');
-// };
-
-
-/*************************** */
 
 //запускаем основную функцию валидации
 enableValidation();
