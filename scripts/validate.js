262a57933bc6a228afe1659ce6a484b089d56515
Sergey Grigorash <adviser@bk.ru>
2023-04-24 10:20:38 +0500

    feat: ввести и переименовать объект config

--- scripts/validate.js
+++ scripts/validate.js
@@ -95,4 +95,11 @@ function toggleButtonState(inputList, buttonElement) {
 
 
 //запускаем основную функцию валидации
-enableValidation();
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__submit',
+  inactiveButtonClass: 'popup__submit_disabled',
+  inputErrorClass: 'popup__input_invalid',
+  errorClass: 'popup__error_visible'
+});
