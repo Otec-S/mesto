0e412e3ff4505881bc9a0e7db784aed87fc09f8a
Sergey Grigorash <adviser@bk.ru>
2023-06-06 10:41:20 +0500

    ref: настроить webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,14 @@ module.exports = {
 
   module: {
     rules: [
-      // правила для обработки js, html и других файлов
+      {
+        // регулярное выражение, которое ищет все js файлы
+        test: /\.js$/,
+        // при обработке этих файлов нужно использовать babel-loader
+        use: 'babel-loader',
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
+        exclude: '/node_modules/'
+      },
 
       // добавьте ещё одно правило:
       {
@@ -22,13 +29,20 @@ module.exports = {
         // при обработке этих файлов нужно использовать
         // MiniCssExtractPlugin.loader и css-loader
         use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader'
-        }]
+          loader: 'css-loader',
+          // добавьте объект options
+          options: { importLoaders: 1 }
+        },
+          // Добавьте postcss-loader
+          'postcss-loader']
       },
       {
         // регулярное выражение, которое ищет все файлы с такими расширениями
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource'
+        type: 'asset/resource',
+        generator: {
+          filename: 'images and fonts/[hash][ext]'
+        }
       },
     ],
   },
