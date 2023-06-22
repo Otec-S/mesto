d938fa2bc46a2fe7b08944cb980cda290991c9c3
Sergey Grigorash <adviser@bk.ru>
2023-06-22 10:57:31 +0500

    feat: сверстать попап для подтверждения удаления

---
+++ src/components/PopupWithConfirmation.js
@@ -0,0 +1,14 @@
+import Popup from "./Popup.js";
+
+// export default class PopupWithConfirmation extends Popup {
+//   constructor(popupSelector, submitForm) {
+//     super(popupSelector);
+//     //Кроме селектора попапа принимает в конструктор колбэк сабмита формы
+//     this._submitForm = submitForm;
+//     //общая ссылка на форму из попапа (есть в обеих формах)
+//     this._formElement = this._popup.querySelector('.popup__form');
+//   }
+
+
+
+// }
