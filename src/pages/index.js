eff4818b5ebe4406e5569cf79df089ab775a1ed4
Sergey Grigorash <adviser@bk.ru>
2023-06-29 09:41:01 +0500

    fix: правильное удаление через ДА на попапе

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,12 +36,13 @@ function makeElementOfClassCard(data) {
     {
       confirmDelete: () => {
         confirmPopUp.open();
-        confirmPopUp.confirmDeleteCard().addEventListener('click', () => {
+        confirmPopUp.confirmDeleteCard().addEventListener('click', function deleteCurrentCard() {
           api.deleteCard(data._id)
             .then(() => { card.handleTrashCanToRemoveCard(); })
             .catch((err) => {
               console.log('Что-то пошло не так', err)
             });
+          confirmPopUp.confirmDeleteCard().removeEventListener('click', deleteCurrentCard);
           confirmPopUp.close();
         })
       }
