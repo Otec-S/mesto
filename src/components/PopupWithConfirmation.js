601c317b7b63f63b66d331ac0ef6b25ce08126cc
Sergey Grigorash <adviser@bk.ru>
2023-06-30 09:53:19 +0500

    все равно не работает

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -1,11 +1,10 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithConfirmation extends Popup {
-  constructor(popupSelector, submitCallback) {
-    super(popupSelector);
-    this.submitCallback = submitCallback;
-
-  }
+  // constructor(popupSelector, submitCallback) {
+  //   super(popupSelector);
+  //   this.submitCallback = submitCallback;
+  // }
 
   //ссылка на кнопку "ДА"
   confirmDeleteCard() {
@@ -13,12 +12,16 @@ export default class PopupWithConfirmation extends Popup {
     return deleteConfirmButton;
   }
 
-  _handleSubmitCallback() {
-    this.submitCallback();
+  _handleSubmitCallback(action) {
+    this.submitCallback = action;
   }
 
   setEventListeners() {
     super.setEventListeners();
     this.confirmDeleteCard.addEventListener('click', this._handleSubmitCallback);
   }
+
+
 }
+
+
