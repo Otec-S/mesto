e6172881368cf75ab75f25b6918e0a9f74a62b78
Sergey Grigorash <adviser@bk.ru>
2023-07-05 11:20:30 +0500

    ref: simplified the validation class

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -18,15 +18,15 @@ class FormValidator {
     this._buttonElement = this._formElement.querySelector(this._submitButtonSelector);
   }
 
-  enableValidation(config) {
+  enableValidation() {
 
     //проверка состояния кнопки при первой загрузке страницы.
-    this._toggleButtonState(config, this._inputList, this._buttonElement);
+    this._toggleButtonState();
 
     //чистим форму после ее отправки и тут же проверяем кнопку на валидность, делая ее невалидной
     this._formElement.addEventListener('submit', () => {
       this._formElement.reset();
-      this._toggleButtonState(config, this._inputList, this._buttonElement);
+      this._toggleButtonState();
     });
 
     //проходим по массиву всех инпутов inputList
@@ -34,36 +34,34 @@ class FormValidator {
       //вешаем на каждый input слушатель ввода
       inputElement.addEventListener('input', () => {
         //вызываем функцию checkInputValidity
-        this._checkInputValidity(config, this._formElement, inputElement);
-        //вызываем функцию toggleButtonState в теле обработчика события input. Передаём ей массив полей формы и элемент кнопки. Такой вызов проверит состояние кнопки при каждом изменении символа в любом из полей.
-        this._toggleButtonState(config, this._inputList, this._buttonElement);
+        this._checkInputValidity(inputElement);
+        //вызываем функцию toggleButtonState в теле обработчика события input. Такой вызов проверит состояние кнопки при каждом изменении символа в любом из полей.
+        this._toggleButtonState();
       });
     });
   };
 
   //ВАЛИДАЦИЯ ПОЛЕЙ ВВОДА
   //функция вызывает показ ошибок при невалидации
-  _checkInputValidity(config, formElement, inputElement) {
+  _checkInputValidity(inputElement) {
     if (!inputElement.validity.valid) {
-      this._showInputError(config, this._formElement, inputElement, inputElement.validationMessage);
+      this._showInputError(inputElement, inputElement.validationMessage);
     } else {
-      this._hideInputError(config, this._formElement, inputElement);
+      this._hideInputError(inputElement);
     }
   };
 
   //показываем невалидное состаяние
-  _showInputError(config, formElement, inputElement, errorMessage) {
+  _showInputError(inputElement, errorMessage) {
     const errorElement = this._formElement.querySelector(`#error-${inputElement.id}`);
-    inputElement.classList.add(config.inputErrorClass);
+    inputElement.classList.add(this._inputErrorClass);
     errorElement.textContent = errorMessage;
-    // errorElement.classList.add('form__input-error_active');
   };
 
   //показываем валидное состаяние
-  _hideInputError(config, formElement, inputElement) {
+  _hideInputError(inputElement) {
     const errorElement = this._formElement.querySelector(`#error-${inputElement.id}`);
-    inputElement.classList.remove(config.inputErrorClass);
-    // errorElement.classList.remove('form__input-error_active');
+    inputElement.classList.remove(this._inputErrorClass);
     errorElement.textContent = '';
   };
 
@@ -73,23 +71,22 @@ class FormValidator {
   // Если поле не валидно, колбэк вернёт true
   // Обход массива прекратится и вся функция
   // hasInvalidInput вернёт true
-  _hasInvalidInput(inputList) {
+  _hasInvalidInput() {
     return this._inputList.some((inputElement) => {
       return !inputElement.validity.valid;
     });
   };
 
   //функция переключения состояния кнопок отправки
-  // Первый параметр — массив полей, второй — кнопка.
-  _toggleButtonState(config, inputList, buttonElement) {
+  _toggleButtonState() {
     if (this._hasInvalidInput(this._inputList)) {
       // сделай кнопку неактивной
       this._buttonElement.setAttribute('disabled', '');
-      this._buttonElement.classList.add(config.inactiveButtonClass);
+      this._buttonElement.classList.add(this._inactiveButtonClass);
     } else {
       // иначе сделай кнопку активной
       this._buttonElement.removeAttribute('disabled');
-      this._buttonElement.classList.remove(config.inactiveButtonClass);
+      this._buttonElement.classList.remove(this._inactiveButtonClass);
     }
   };
 };
